d99265a58b4ebdfb1cb3af91268e36b0b30484db
melvinchia3636 <melvinchia623600@gmail.com>
2022-07-16 05:32:57 +0800

    added dist

---
+++ dist/templates/react/src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div className="w-full h-screen flex items-center justify-center text-2xl">Hello World</div>;
+}
+
+export default App;
---
+++ dist/templates/vanilla/src/main.ts
@@ -0,0 +1,5 @@
+import './style.css';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <h1 class="text-2xl">Hello World</h1>
+`;
